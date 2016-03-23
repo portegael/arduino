45e8bfa22b041c39e2d7dab6c79b0a8ecd133926
Gael Porte <porte.gael@hotmail.fr>
2016-03-23 23:16:00 +0100

    Update correct commands to compile files

--- 02_ultrasonic_detector/cmd.sh
+++ 02_ultrasonic_detector/cmd.sh
@@ -1,2 +1,7 @@
-avr-g++ -I/opt/arduino-1.6.8/hardware/tools/avr/avr/include -I/opt/arduino-1.6.8/hardware/arduino/avr/cores/arduino/ -Wall -Os -DF_CPU=16000000UL -mmcu=atmega2560 main.cpp -o main.o
 avr-g++ -I/opt/arduino-1.6.8/hardware/tools/avr/avr/include -I/opt/arduino-1.6.8/hardware/arduino/avr/cores/arduino/ -Wall -Os -DF_CPU=16000000UL -mmcu=atmega2560 ledManager.cpp -c
+avr-g++ -I/opt/arduino-1.6.8/hardware/tools/avr/avr/include -I/opt/arduino-1.6.8/hardware/arduino/avr/cores/arduino/ -Wall -Os -DF_CPU=16000000UL -mmcu=atmega2560 main.cpp -c
+
+$make
+avr-g++  -Wall -Os -DF_CPU=16000000UL -mmcu=atmega2560  main.o ledManager.o -o detector
+
+$sudo make upload
